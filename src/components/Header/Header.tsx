06179acbf51210d10828499c0baf34e40ee5a9d0
Nafil Arzzam <nafilarzzam19@gmail.com>
2023-08-06 20:07:16 +0530

    Add React Icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
+import ReactIcon from "../../assets/ReeactIcon";
+
 function Header() {
   return (
     <header className="w-[66rem] flex items-center justify-between mb-16">
-      <img src="logo512.png" alt="React logo" />
+      <ReactIcon className="w-56" />
       <h1>The React Quiz</h1>
     </header>
   );

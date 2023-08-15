a909bed8e32b95b0bf0eb339e7eb17957d1b0841
Nafil Arzzam <nafilarzzam19@gmail.com>
2023-08-15 15:37:44 +0530

    Create Progress bar

---
+++ src/components/Progress/Progress.tsx
@@ -0,0 +1,33 @@
+interface IProgressProps {
+  index: number;
+  questionLength: number;
+  points: number;
+  totalPoints: number;
+  clickAnswer?: number | null;
+}
+
+const Progress = ({
+  index,
+  questionLength,
+  points,
+  totalPoints,
+  clickAnswer,
+}: IProgressProps) => {
+  return (
+    <header className="mb-16 grid justify-between gap-5 grid-cols-2 text-2xl text-color-medium">
+      <progress
+        className="w-full h-3 grid col-span-full"
+        max={questionLength}
+        value={index + Number(clickAnswer !== null)}
+      />
+      <p className="text-left">
+        Question <strong>{index}</strong>/ {questionLength}
+      </p>
+      <p className="text-right">
+        <strong>{points}</strong> / {totalPoints} Points
+      </p>
+    </header>
+  );
+};
+
+export default Progress;
